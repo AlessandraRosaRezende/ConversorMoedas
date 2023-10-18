0b26cd16384ac18278743da6da2068969444dff5
AlessandraRosaRezende <alessandra.rosarezende@gmail.com>
2023-10-18 07:25:34 -0300

    docs: incluir swagger

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ const app = express();
 
 app.use(express.json());
 
+/**
+ * @swagger
+ * /converter/BRL/{amount}:
+ *   get:
+ *     summary: Obtém informações sobre a API de conversão de moedas.
+ *     parameters:
+ *       - in: path
+ *         name: amount
+ *         required: true
+ *         description: O valor em BRL a ser convertido.
+ *     responses:
+ *       200:
+ *         description: Documentação API
+ */
 app.get("/converter/BRL/:amount", currencyConverterController.convert);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,31 @@
 const app = require("./app");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const PORT = 3001;
+
+const swaggerDefinition = {
+	openapi: "3.0.0",
+	info: {
+		title: "API de Conversão de Moedas",
+		version: "1.0.0",
+		description: "Uma API para conversão de moedas.",
+	},
+};
+
+const options = {
+	swaggerDefinition,
+	servers: [
+		{
+			url: "http://localhost:3001",
+		},
+	],
+	apis: ["src/app.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const server = app.listen(PORT, () =>
 	console.log(`API de Conversão de Moedas rodando em http://localhost:${PORT}`)

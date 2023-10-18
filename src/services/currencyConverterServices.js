a9c425a0c632d3707f325726f7ed9f2476999d06
AlessandraRosaRezende <alessandra.rosarezende@gmail.com>
2023-10-18 06:41:35 -0300

    feat: inclui as regras de negócio

--- src/services/currencyConverterServices.js
+++ src/services/currencyConverterServices.js
@@ -1,4 +1,47 @@
 const request = require("../models/request");
+const { DateTime } = require("luxon");
+
+const DISCOUNT_PERCENTAGE_10 = 10;
+const DISCOUNT_PERCENTAGE_15 = 15;
+const DISCOUNT_PERCENTAGE_5 = 5;
+const VALUE_DISCOUNT_5 = 3000;
+const VALUE_DISCOUNT_15 = 2000;
+
+const applyDiscount = (amount) => {
+	let discountPercentage = 0;
+
+	const currentTime = DateTime.now().setZone("America/Sao_Paulo");
+	const isSaturdayNight = currentTime.weekday === 6 && currentTime.hour >= 22;
+	const isSundayMorning = currentTime.weekday === 7 && currentTime.hour < 5;
+
+	if (isSaturdayNight || isSundayMorning) {
+		discountPercentage =
+			amount > VALUE_DISCOUNT_15
+				? DISCOUNT_PERCENTAGE_15
+				: DISCOUNT_PERCENTAGE_10;
+	}
+
+	if (isMondayToFriday(currentTime) && amount > VALUE_DISCOUNT_5) {
+		discountPercentage = DISCOUNT_PERCENTAGE_5;
+	}
+
+	const calcFinal = calFinal(discountPercentage, amount);
+
+	return calcFinal;
+};
+
+const isMondayToFriday = (currentTime) => {
+	return (
+		currentTime.weekday >= 1 &&
+		currentTime.weekday <= 5 &&
+		currentTime.hour >= 19 &&
+		currentTime.hour <= 23
+	);
+};
+
+const calFinal = (discountPercentage, amount) => {
+	return (1 - discountPercentage / 100) * amount;
+};
 
 const convertCurrency = async (amount) => {
 	const currencyData = await request.fetchCurrencyData();
@@ -13,7 +56,8 @@ const convertCurrency = async (amount) => {
 				return { error: "Invalid currency data" };
 			}
 
-			conversionResults[currency] = (amount / rate).toFixed(2);
+			const convertedAmount = (applyDiscount(amount) / rate).toFixed(2);
+			conversionResults[currency] = convertedAmount;
 		});
 
 		return {

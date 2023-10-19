f9dce621892e92cfe9652980090ff9f77bd97a26
AlessandraRosaRezende <alessandra.rosarezende@gmail.com>
2023-10-18 22:56:41 -0300

    refactor: configuracao swagger em arquivo proprio

--- src/server.js
+++ src/server.js
@@ -1,29 +1,9 @@
 const app = require("./app");
-const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swaggerConfig");
+require('dotenv').config();
 
-const PORT = 3001;
-
-const swaggerDefinition = {
-	openapi: "3.0.0",
-	info: {
-		title: "API de Conversão de Moedas",
-		version: "1.0.0",
-		description: "Uma API para conversão de moedas.",
-	},
-};
-
-const options = {
-	swaggerDefinition,
-	servers: [
-		{
-			url: "http://localhost:3001",
-		},
-	],
-	apis: ["src/app.js"],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+const PORT = process.env.PORT;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
